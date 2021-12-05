fc5a5158edfe9a5a466ef6d484ab55e168e567b4
Emil <limezakiev@mail.ru>
2021-12-06 00:59:59 +0300

    Improved Chapter 4 Lab 1

--- ClassWork 01.10.21/ClassWork 01.10.21/kernel.cu
+++ ClassWork 01.10.21/ClassWork 01.10.21/kernel.cu
@@ -1,71 +1,43 @@
 ﻿#include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <cmath>
 #include <stdio.h>
-#include <stdlib.h>
-#include <cuda.h>
-#include <curand.h>
-#include <time.h>
-#include <math.h>
-#include <vector>
-#include<iostream>
 
+#define N 1000
+#define BASE_TYPE short
 
-
-__global__ void CalculatePi(int *niter, int *count, double *x, double *y)
+__device__ bool IsApropriate(double *x, double *y)
 {
-    printf("niter = %d\n", *niter);
-    for (int i = 0; i < *niter; ++i)
-    {
-        double vectorLength = (x[i] * x[i]) + (y[i] * y[i]);
+    bool result = (*x) * (*x) + (*y) * (*y) <= 1;
+    return result;
+}
 
-        if (vectorLength <= 1)
-            ++ *count;
-    }
-    printf("count = %d\n", *count);
+__global__ void CalculatePI(BASE_TYPE *d_a) 
+{
+    double x = (double)blockIdx.x / N;
+    double y = (double)threadIdx.x / N;
+    d_a[blockIdx.x * N + threadIdx.x] = IsApropriate(&x, &y);
 }
 
 int main()
 {
-    const int niter = 50000;
-    int i;
-    int count = 0;
-    double pi;
-    int *dev_niter, *dev_count;
-    int size = sizeof(int);
-    double x[niter];
-    double *dev_x;
-    double y[niter];
-    double *dev_y;
+    int size = N * N;
+    BASE_TYPE *a = new BASE_TYPE[size];
+    BASE_TYPE *d_a;
+    int counter = 0;
+
+    cudaMalloc((void**)&d_a, size * sizeof(BASE_TYPE));
+
+    CalculatePI <<< N, N >>>(d_a);
     
-    const size_t x_size = sizeof(double) * size_t(niter);
+    cudaMemcpy(a, d_a, size * sizeof(BASE_TYPE), cudaMemcpyDeviceToHost);
 
-    srand(time(NULL));
-
-    for (int i = 0; i < niter; i++)
+    for (int i = 0; i < size; i++) 
     {
-        x[i] = (double)rand() / RAND_MAX;
-        y[i] = (double)rand() / RAND_MAX;
-
+        counter += a[i];
     }
+    printf("pi = %f\n", (double)counter * 4 / size);
 
-    cudaMalloc((void**)&dev_niter, size);
-    cudaMalloc((void**)&dev_count, size);
-
-    cudaMalloc((void**)&dev_x, x_size);
-    cudaMalloc((void**)&dev_y, x_size);
-
-    cudaMemcpy(dev_niter, &niter, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_count, &count, size, cudaMemcpyHostToDevice);
-
-    cudaMemcpy(dev_x, x, x_size, cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_y, y, x_size, cudaMemcpyHostToDevice);
-
-    CalculatePi<<< 1, 1 >>> (dev_niter, dev_count, dev_x, dev_y);
-
-    cudaMemcpy(&count, dev_count, size, cudaMemcpyDeviceToHost);
-
-    pi = ((double)count / (double)niter) * 4.0;
-    std::cout << "Pi number: " << pi << std::endl;
-
+    cudaFree(d_a);
     return 0;
 }
